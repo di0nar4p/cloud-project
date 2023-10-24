b830e67292815fda108b1f39626d06ee13356d98
Glauco Santos <glaucoplay@live.com>
2023-10-24 12:20:49 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+
+app.use(cors());
 
 app.get("/", (request, response)=>{
     response.json({
